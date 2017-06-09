eb66bec16fbb6494321f6f067d47ca8e9a8587f7
admin <a@m.com>
2017-06-09 23:13:00 +0100

    First commit after creating git repo

---
+++ src/main/java/compareLength.java
@@ -0,0 +1,16 @@
+import java.util.Comparator;
+
+/**
+ * Created by admin on 6/9/2017.
+ */
+class compareLength implements Comparator<String> {
+    public int compare(String o1, String o2) {
+        if (o1.length() > o2.length()) {
+            return 1;
+        } else if (o1.length() < o2.length()) {
+            return -1;
+        } else {
+            return o1.compareTo(o2);
+        }
+    }
+}
---
+++ src/main/java/main.java
@@ -0,0 +1,34 @@
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * Created by admin on 6/9/2017.
+ */
+public class main {
+
+
+
+    public static void main(String[] args){
+        System.out.println("hello world");
+
+        SortedSet<String> inputWords = new TreeSet<String>(new compareLength());
+        inputWords.add("BB");
+        inputWords.add("CCC");
+        inputWords.add("A");
+        inputWords.add("A");
+        inputWords.add("EEEEE");
+        inputWords.add("DDDD");
+
+        Iterator<String> i = inputWords.iterator();
+        while (i.hasNext()) {
+            String currentWord = i.next(); // must be called before you can call i.remove()
+            System.out.println(currentWord);
+            System.out.println("----------------");
+            i.remove();
+        }
+
+    }
+
+}
