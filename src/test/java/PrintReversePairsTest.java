e2b472541081d0836c7c689269ea4e0dc44b9389
admin <a@m.com>
2017-06-11 01:32:28 +0100

    tests have been declared

---
+++ src/test/java/PrintReversePairsTest.java
@@ -0,0 +1,37 @@
+import org.junit.Test;
+
+/**
+ * Created by admin on 6/11/2017.
+ */
+public class PrintReversePairsTest {
+
+    @Test
+    public void testFileDoesNotExist(){
+
+    }
+
+    @Test
+    public void testFileNotAccessable(){
+
+    }
+
+    @Test
+    public void testFileNotATextFile(){
+
+    }
+
+    @Test
+    public void testBlankTextFile(){
+
+    }
+
+    @Test
+    public void testFileWithDuplicateWords(){
+
+    }
+
+    @Test
+    public void testWordsAreSortedByLength(){
+
+    }
+}
---
+++ src/test/java/supportClasses/compareLengthTest.java
@@ -0,0 +1,20 @@
+package supportClasses;
+
+import org.junit.Test;
+
+/**
+ * Created by admin on 6/11/2017.
+ */
+public class compareLengthTest {
+
+    @Test
+    public void testFirstStringLonger(){
+
+    }
+
+    @Test
+    public void testSecondStringLonger(){
+        
+    }
+
+}
